d3e12d0f29f64a6e7e9741668ac26cda008d6369
LeoNaab <leownaab@gmail.com>
2025-12-08 16:35:18 -0800

    cleaned up my section

--- reflection_obstruction/xue_2.py
+++ reflection_obstruction/xue_2.py
@@ -1,22 +1,18 @@
-from skimage.registration import phase_cross_correlation
-import numpy as np
+import cv2 as cv
 import imageio
-from PIL import Image
 import numpy as np
 from numpy._core.numerictypes import uint8
+from PIL import Image
+from scipy.ndimage import convolve, gaussian_filter, shift
 from skimage.color import rgb2gray
-from scipy.ndimage import gaussian_filter
-from scipy.ndimage import convolve
-from scipy.ndimage import shift
-import cv2 as cv
+from skimage.registration import phase_cross_correlation
 
-
-video_path = 'images/bright_vulture.MOV'
-reader = imageio.get_reader(video_path, 'ffmpeg')
+video_path = "images/hawk.MOV"
+reader = imageio.get_reader(video_path, "ffmpeg")
 
 frames = []
 for i, frame in enumerate(reader):
-    if i < 30 and i > 23 and i:
+    if i < 30 and i > 15 and i:
         frames.append(frame)
 
 canny_frames = []
@@ -28,26 +24,26 @@ for frame in frames:
 # Getting motion
 def get_shift(img1, img2):
     shift, error, diffphase = phase_cross_correlation(
-        reference_image=img1,
-        moving_image=img2,
-        upsample_factor=10
+        reference_image=img1, moving_image=img2, upsample_factor=10
     )
     motion_y, motion_x = shift[:2]
     return motion_y, motion_x
+
 
 def simple_avg(frames):
     stacked_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3), dtype=float)
     for i in range(0, len(frames), 1):
         stacked_frame = np.add(stacked_frame, frames[i])
 
-    stacked_frame = (stacked_frame/len(frames)).astype(uint8)
+    stacked_frame = (stacked_frame / len(frames)).astype(uint8)
     pil_img = Image.fromarray(stacked_frame)
     pil_img.show()
     pil_img = Image.fromarray(frames[0])
     pil_img.show()
 
-def min_images(frames, canny_frames, ref_index=len(frames)//2):
-    min_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3),dtype=uint8) + 255
+
+def min_images(frames, canny_frames, ref_index=len(frames) // 2):
+    min_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3), dtype=uint8) + 255
     for i in range(0, len(frames), 1):
         motion_y, motion_x = get_shift(canny_frames[ref_index], canny_frames[i])
         shifted = shift(frames[i], (motion_y, motion_x, 0))
@@ -59,10 +55,9 @@ def min_images(frames, canny_frames, ref_index=len(frames)//2):
     pil_img.show()
 
 
-
 # Stacking images:
-def stack_images(frames, canny_frames = None, ref_index=3):
-    stacked_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3),dtype=float)
+def stack_images(frames, canny_frames=None, ref_index=3):
+    stacked_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3), dtype=float)
     for i in range(0, len(frames), 1):
         if canny_frames == None:
             motion_y, motion_x = get_shift(frames[3], frames[i])
@@ -72,7 +67,7 @@ def stack_images(frames, canny_frames = None, ref_index=3):
         shifted = shift(frames[i], (motion_y, motion_x, 0))
         stacked_frame = np.add(stacked_frame, shifted)
 
-    stacked_frame = (stacked_frame/len(frames)).astype(uint8)
+    stacked_frame = (stacked_frame / len(frames)).astype(uint8)
     pil_img = Image.fromarray(stacked_frame)
     pil_img.show()
     pil_img = Image.fromarray(frames[3])
@@ -87,13 +82,8 @@ pil_img = Image.fromarray(canny_frames[3])
 pil_img.show()
 
 
-
-
-
-
-
-    # pil_img = Image.fromarray(shifted)
-    # pil_img.show()
+# pil_img = Image.fromarray(shifted)
+# pil_img.show()
 
 # new_min_frame = np.zeros((frames[0].shape[0], frames[0].shape[1], 3),dtype=uint8) + 255
 # for i in range(0, len(frames), 1):
@@ -103,13 +93,13 @@ pil_img.show()
 #     shifted = shift(frames[i], (motion_y, motion_x, 0))
 
 #     min_frame = np.minimum(new_min_frame, shifted)
-    # stacked_frame = np.add(min_frame, shifted)
-    #change shifted to float32:
-    # shifted.
+# stacked_frame = np.add(min_frame, shifted)
+# change shifted to float32:
+# shifted.
 
 
-    # pil_img = Image.fromarray(shifted)
-    # pil_img.show()
+# pil_img = Image.fromarray(shifted)
+# pil_img.show()
 
 
 # stacked_frame = (stacked_frame/len(frames)).astype(uint8)
